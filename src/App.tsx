33e3f6d580f22ce0cdda36df580736dd759d2564
Tadas A. <00tadas@gmail.com>
2025-08-26 00:24:39 +0300

    add tw

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="w-full flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-rose-500">Sports Standings</h1>
+    </div>
+  );
 }
 
 export default App;
